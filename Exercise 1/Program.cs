6c63cbd3c9aad3dd2fc9b6ab39d9e9eb3fafbb85
Eduard <dirol2016@list.ru>
2022-04-25 12:18:10 +0500

    1

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Напишите число: ");
+int _number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Возведем в степень: ");
+int _numberPow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Получается " + Pow(_number, _numberPow));
+
+int Pow(int number, int pow){
+    int result = 1;
+    for(int i = 0; i < pow; i++){
+        result *= number;
+    }
+    return result;
+}
