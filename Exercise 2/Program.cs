6c7db0f2ab05f2d054ccc77e674f924ebc640294
Eduard <dirol2016@list.ru>
2022-04-25 12:19:31 +0500

    2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Напишите число: ");
+string _strNumber = Console.ReadLine();
+
+int _sum = 0;
+for(int i = 0; i < _strNumber.Length; i++){
+    _sum += Convert.ToInt32(_strNumber[i].ToString());
+}
+
+Console.WriteLine("Сумма цифр: " + _sum);
+
