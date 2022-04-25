907928067fbc73cb8cf226a6e437f39a6d52dfac
Eduard <dirol2016@list.ru>
2022-04-25 12:19:49 +0500

    3

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите числа через запятую: ");
+string _numbers = Console.ReadLine();
+_numbers = _numbers.Replace(" ", "");
+string[] _arrayNumbers = _numbers.Split(new char[] {','});
+
+for(int i = 0; i < _arrayNumbers.Length; i++){
+    Console.Write($"{_arrayNumbers[i]} ");
+}
